66b6ab9bdff3941f7ee241045ede606638ef43c8
Ecoit-Hugo DESSAUW <dessauw.hugo@gmail.com>
2022-07-22 11:01:50 +0200

    Ajout page profil/mentions légales/ politique confidentialitee

---
+++ src/Controller/SandrineProfilController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SandrineProfilController extends AbstractController
+{
+    #[Route('/sandrine/profil', name: 'app_sandrine_profil')]
+    public function index(): Response
+    {
+        return $this->render('sandrine_profil/sandrine_profil.html.twig', [
+            'controller_name' => 'SandrineProfilController',
+        ]);
+    }
+}
